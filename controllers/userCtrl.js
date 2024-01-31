b104f7e0fcb7aeade5625f843783a3dd1c42c861
Bibek <bistabibek9@gmail.com>
2024-01-31 22:05:47 +0545

    changes on regiter controller

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -6,33 +6,6 @@ const appointmentModel = require("../models/appointmentModel");
 const moment = require("moment");
 
 
-//register controller
-// const registerController = async (req, res) => {
-//   try {
-//     const exisitingUser = await userModel.findOne({ email: req.body.email });
-//     if (exisitingUser) {
-//       return res
-//         .status(200)
-//         .send({ message: "User Already Exist", success: false });
-//     }
-    
-//     const password = req.body.password;
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
-//     req.body.password = hashedPassword;
-//     const newUser = new userModel(req.body);
-//     await newUser.save();
-//     res.status(201).send({ message: "Register Sucessfully", success: true });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({
-//       success: false,
-//       message: `Register Controller ${error.message}`,
-//     });
-//   }
-// };
-
-
 const registerController = async (req, res) => {
   try {
     // Check if required fields are empty
